6145e8dd3d38f79507b47a242b8c3e2f84db95ad
Mayank Kashyap <mayankkashyap@Mayanks-MacBook-Pro.local>
2024-08-23 15:42:43 +0530

    Change server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config(); // Make sure this line is present
+require('dotenv').config();
 
 const app = express();
 
